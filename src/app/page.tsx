9cbec918ac2037b2ef7e01e55a39085caab0a088
Kostrica <romiorw@ukr.net>
2024-05-02 15:06:43 +0200

    Fixed main title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <div className="flex min-h-screen h-screen flex-col items-center justify-between py-12 px-3 md:p-12">
       <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 from-zinc-200 backdrop-blur-2xl bg-gradient-to-b pt-3 pb-36 lg:static lg:w-auto lg:py-0 lg:border-none lg:bg-none">
-        <Header title="Welcome to my Nest.js app!" />
+        <Header title="Welcome to my Next.js app!" />
       </div>
       <main className="flex h-full flex-col items-center justify-between lg:py-6">
         <div className="sticky top-14 lg:static">
